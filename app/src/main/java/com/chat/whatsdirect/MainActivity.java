7a5273a20436fa1951b1b7a478a88fb5aee034ae
Jigyansha Jeevan Rout <62542350+sudo-xda@users.noreply.github.com>
2020-07-02 09:05:47 +0530

    All country support

--- app/src/main/java/com/chat/whatsdirect/MainActivity.java
+++ app/src/main/java/com/chat/whatsdirect/MainActivity.java
@@ -9,11 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.hbb20.CountryCodePicker;
+
 public class MainActivity extends AppCompatActivity {
+
 
     private EditText Mobile;
     private EditText Message;
     private Button etB;
+    private CountryCodePicker cCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +26,14 @@ public class MainActivity extends AppCompatActivity {
         Mobile=findViewById(R.id.etM);
         Message=findViewById(R.id.etT);
         etB =findViewById(R.id.etB);
+        cCode = findViewById(R.id.cCode);
         etB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String phone = Mobile.getText().toString().trim();
                 String messages = Message.getText().toString().trim();
-                String url = "https://wa.me/"+91 + phone + "?text=" + messages ;
+                String ccode = cCode.getSelectedCountryCode();
+                String url = "https://wa.me/"+ ccode + phone + "?text=" + messages ;
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
                 startActivity(intent);
